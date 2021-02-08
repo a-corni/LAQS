10333031b651a403d32d7cbae2556b93c3aa0e2e
acorni <antoine.cornillot@student-cs.fr>
2021-02-08 19:04:08 +0100

    Final update

--- Laser for transport/Monte-Carlo simulation/RKMCPython/MyMCPython_para/MC_ivp_2/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Suite of ODE solvers implemented in Python."""
-from .MC_ivp import solve_ivp
-from .MC_rk import RK45
-from .MC_common import OdeSolution
-from .MC_base import DenseOutput, OdeSolver
--- Laser for transport/Monte-Carlo simulation/RKMCPython/MyMCPython_para/__intro__.py
+++
@@ -1,1 +0,0 @@
-from .ftestparrallelization import f
